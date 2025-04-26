632aad972174ee67f6baf606c758db332749ee7c
character81 <diegojb@gmail.com>
2025-04-26 12:54:12 -0300

    Delete Bakersoft directory - Nueva estructura de archivos

--- Bakersoft/materiaprima/vista/borraMP.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("../controlador/controladorMP.php");
-    $obj = new controladorMP();
-
-    #var_dump($_POST);
-    $obj->borrar($_POST['borrarID']);
-    ?>
--- Bakersoft/materiaprima/vista/modificarMP.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("../controlador/controladorMP.php");
-    $obj = new controladorMP();
-
-   #var_dump($_POST);
-       $res = $obj->actualizar($_POST['editid'],$_POST['editnombre'],$_POST['editstockminimo'],$_POST['editstockactual']);
-
-?>
